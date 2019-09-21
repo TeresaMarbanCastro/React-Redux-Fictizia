ad6b52a51bd61a292c8822fa55dedc7ccc044829
React <teresamarbancastro@gmail.com>
2019-09-21 14:44:40 +0200

    to do list edition

--- render-components/src/components2/ListContainer.js
+++ render-components/src/components2/ListContainer.js
@@ -4,6 +4,7 @@ import List from './List';
 import ListItem from './ListItem';
 import AddButton from './AddButton';
 import RemoveButton from './RemoveButton';
+import EditButton from './EditButton';
 
 class ListContainer extends React.Component {
     constructor(props) {
@@ -29,13 +30,34 @@ class ListContainer extends React.Component {
         })
     }
 
+    // editTask(e) {
+    //     this.setState({
+    //         tasks: this.state.tasks.splice(this.state.text, index, this.state.text)
+    //     })
+    // }
+
+    // onChangeTask = (id, text) => {
+    // let tasks = this.state.tasks.map(e => {
+    //     if(e.id === id) e.text = text
+    //         return e
+    //     })
+    //     this.setState({})
+    // } Hacer la edición, necesitaremos un array con objetos con id y text dentro. 
+
+    // 1. Cambiar estructura para que tenga id
+    // 2. Se le pasa el objeto entero (id y texto) en list item y se le pasa al input props.task.text
+    // 3. Crear función onChange qeu llama a la del padre con el evento de cuando estamos escribiendo y el id de esa tarea. 
+    // 4. En la función onChangeTask se hace un map. Si la tarea que estoy recorriendo, el id es igula al id que me viene,  lo devuelvo
+    // 5. Añadir Mathrandom para dar el id
+
     render(){
         return (
             <>
                 <h2>To Do List</h2>
-                <InputText onChange={(e) => this.setState({text: e.target.value})} />
+                <InputText valueonChange={(e) => this.setState({text: e.target.value})} />
                 <AddButton onClick={this.addTask} />
                 <RemoveButton onClick={this.removeTask} />
+                <EditButton onClick={this.editTask} />
 
                 <List>
                     {this.state.tasks.map((element) => <ListItem key={element}>{element}</ListItem>)}
